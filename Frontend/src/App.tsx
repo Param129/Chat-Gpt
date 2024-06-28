5c06e56fda2305136cd950c7128d7935c1bf2f89
Param Tomar <paramtomar81@gmail.com>
2024-06-28 10:52:07 +0530

    prepared Routes and all

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,30 @@
+import Header from "./components/Header"
+import {Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import Chat from "./pages/Chat"
+
 
 function App() {
 
+   return (
 
-   return <main> hello param</main>
+      <main>
+
+        <Header/>
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/chat" element={<Chat/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </main>
+
+   )
 }
 
 export default App
