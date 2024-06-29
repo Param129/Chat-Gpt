122be19d70c4f5869fa8a6a2df5be5189160ae80
Param Tomar <paramtomar81@gmail.com>
2024-06-29 11:11:06 +0530

    Navbar

--- Frontend/src/components/shared/NavigationLink.tsx
+++ Frontend/src/components/shared/NavigationLink.tsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom"
 
-const NavigationLink = () => {
+
+type Props = {
+    to : string,
+    bg : string,
+    text : string,
+    textColor : string,
+    onClick ?: ()=>Promise<void>;
+}
+
+
+
+const NavigationLink = (props:Props) => {
   return (
-    <div>
-      
-    </div>
-  )
+        <Link to={props.to}  style={{background:props.bg , color:props.textColor}} className="nav-link"> {props.text} </Link>
+    )
 }
 
 export default NavigationLink
